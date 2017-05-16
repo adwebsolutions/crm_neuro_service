baa519aa473bf172f61001c2e0258d546c82f596
Ignacio Valencia <ignacio.valencia@adweb.mx>
2017-05-16 13:06:17 -0500

    Módulo CRM para subir 16/05/17
    Actualización de módulo

--- models/crm_custom_fields.py
+++ models/crm_custom_fields.py
@@ -7,7 +7,8 @@ class CrmCustomFields(models.Model):
   _inherit = "crm.lead"
 
   neuro_fecha_de_cita  = fields.Date('Fecha de Cita')
-  neuro_seguimiento    = fields.Char(string="Número de Guía")
+  neuro_fecha_llegada  = fields.Datetime('Fecha y hora de Entrega')
+  neuro_guia    = fields.Char(string="Número de Guía")
   neuro_canal          = fields.Selection(selection=[('formulario','Formulario'),('telefono', 'Teléfono'),('email', 'Email'),('facebook', 'Facebook'),('linkedin', 'Linkedin')], string ='Canal',  help="Canal por el cual llegó la oportunidad")
   neuro_origen         = fields.Many2one(comodel_name = 'neuro.origen', string ='Origen',  help="Origen de la oportunidad")
   neuro_evento         = fields.Many2one(comodel_name = 'neuro.evento', string ='Evento')
